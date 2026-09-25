0d954b8253935d88d92905ff6352d832d9b3a6dd
davidseignious <ddseign@gmail.com>
2026-09-24 20:43:22 -0500

    Rotate cache for profile preview and photo editor

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='veramor-shell-v6';
+const CACHE='veramor-shell-v7';
 const CORE=['/','/beta.css','/beta-growth.css','/veramor-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
